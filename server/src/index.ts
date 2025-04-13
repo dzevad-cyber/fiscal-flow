b66c42528e9ba3d1be61cefdf15eb60b24ea77a3
Dzevad Delihodzic <dzevad.del@gmail.com>
2025-04-13 22:32:52 +0200

    feat: finish server setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,6 @@
 import app from './server.ts';
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server listening. ${process.env.NODE_ENV}:${PORT}`)
+);
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,21 @@
+import cookieParser from 'cookie-parser';
 import express from 'express';
+import morgan from 'morgan';
 
 const app = express();
 
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('combined'));
+}
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.json({
+    message: 'Hello World',
+  });
 });
 
 export default app;
